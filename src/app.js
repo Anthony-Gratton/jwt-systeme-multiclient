37765e388019b2a623e93c2bf56bc64027ddfd8f
Anthony <2060246@cstj.qc.ca>
2023-09-07 15:12:08 -0400

    fin cours jeudi semaine 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import database from "./core/database.js";
+import limitRoutes from "./routes/limits.route.js"
+
 
 const app = express();
 database();
+
+app.use(limitRoutes)
 
 export default app;
