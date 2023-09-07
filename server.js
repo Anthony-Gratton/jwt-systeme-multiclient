4f0228f95cf59fea76b12375e0b28a86edfb801a
Anthony <2060246@cstj.qc.ca>
2023-09-07 14:50:08 -0400

    cours jeudi semaine 3

---
+++ server.js
@@ -0,0 +1,15 @@
+import './load-env.js';
+import chalk from "chalk";
+import app from './src/app.js';
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (err) => {
+
+    if (err) {
+        process.exit(1)
+    }
+
+    console.log(chalk.green(`Loading environment from ${process.env.NODE_ENV}`))
+    console.log(chalk.blue(`🦖☄️ Server listening on port ${PORT}`))
+})
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import database from "./core/database.js";
+
+const app = express();
+database();
+
+export default app;
