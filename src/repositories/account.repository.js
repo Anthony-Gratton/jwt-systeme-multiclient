08881df2073a01f5aad02d5cc8067d15d5fba909
Anthony <2060246@cstj.qc.ca>
2023-09-19 17:58:17 -0400

    JWT

--- src/repositories/account.repository.js
+++ src/repositories/account.repository.js
@@ -1,9 +1,46 @@
+import { v4 as uuidv4 } from 'uuid'
+import HttpError from 'http-errors';
 import { Account } from "../models/account.model.js";
+import argon2d from 'argon2';
 
 class AccountRepository {
 
-    create(account) {
+    async login(email, username, password) {
+        const account = await Account.findOne({ $or: [{ email: email }, { username: username }] })
+        if (!account) {
+            return { err: HttpError.Unauthorized() }
+        }
+
+        if (await this.validatePassword(password, account)) {
+            return { account };
+        } else {
+            //bad password
+            return { err: HttpError.Unauthorized() }
+        }
+
+
+
+    }
+
+    generateJWT() {
+
+    }
+
+    //verify password with argon
+    async validatePassword(password, account) {
+
         try {
+            return await argon2d.verify(account.passwordHash, password)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async create(account) {
+        try {
+            account.uuid = uuidv4();
+            account.passwordHash = await argon2d.hash(account.password)
+            delete account.password;
             return Account.create(account);
         } catch (err) {
             throw err;
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+import HttpError from "http-errors";
 import accountRepository from "../repositories/account.repository.js";
 
 const router = express.Router();
@@ -9,7 +10,27 @@ class accountRoutes {
     constructor() {
 
         router.post('/', this.post);
+        router.post('/actions/login', this.login)
 
+
+
+
+    }
+
+    async login(req, res, next) {
+        const { email, username, password } = req.body
+        if ((email && username) || email === "" || username == "") {
+            return next(HttpError.BadRequest('Provide either email or username'))
+        }
+        const result = await accountRepository.login(email, username, password)
+        if (result.account) {
+            let account = result.account.toObject({ getters: false, virtuals: false })
+            //generate jwt tokens
+
+            res.status(201).json(account)
+        } else {
+            return next(result.err)
+        }
     }
 
     async post(req, res, next) {
